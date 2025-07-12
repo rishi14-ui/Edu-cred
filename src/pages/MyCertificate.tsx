a6b600af8dee11aa222efffd72230900351ee7ff
rishi14-ui <rishi.240255@aids.ritchennai.edu.in>
2025-07-12 13:18:10 +0530

    Updated MyCertificate.tsx

--- src/pages/MyCertificate.tsx
+++ src/pages/MyCertificate.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import { Certificate, Download, Eye, Share } from 'lucide-react';
+import { AlignCenterVertical as Certificate, Download, Eye, Share } from 'lucide-react';
 
 const MyCertificate: React.FC = () => {
   const [loading, setLoading] = useState(true);
